ce56dd001e9c0ddfb523a289c144afb009c8b1c6
MH <martinhamon@gmail.com>
2017-03-01 12:01:56 -0300

    Primer commit

--- app/src/main/java/com/mcode/tempsigna/About.java
+++ app/src/main/java/com/mcode/tempsigna/About.java
@@ -11,5 +11,6 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        /*prueba de git*/
     }
 }
